e7595de65603d522127cb6f96db724f828947c34
Kurt Liang <liangke1991@gmail.com>
2023-06-22 20:54:18 +1200

    create pages

---
+++ pages/events/[catagary]/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Page() {
+  return (
+    <div>
+      <h1>aaaaa</h1>
+      <a href="/events/event1">event1</a>
+      <a href="/events/event2">event2</a>
+      <a href="/events/event3">event3</a>
+      <a href="/events/event4">event4</a>
+      <a href="/events/event5">event5</a>
+      <a href="/events/event6">event6</a>
+    </div>
+  )
+}
+
+export default Page
