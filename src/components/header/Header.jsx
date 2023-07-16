1183749553b5a856808aa7da652b9b3e225cc704
Kurt Liang <liangke1991@gmail.com>
2023-07-17 11:32:49 +1200

    wrap a div nest down the header tag

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,31 +5,34 @@ import Link from 'next/link'
 function Header() {
   return (
     <header>
-      <div className="topNav">
-        <Image alt={'logo'} src={'/logo/Logo-k.png'} width={50} height={50} />
-        <nav>
-          <ul>
-            <li>
-              <Link legacyBehavior href="/" passHerf>
-                <a>Home</a>
-              </Link>
-            </li>
-            <li>
-              <Link legacyBehavior href="/events" passHerf>
-                <a>Events</a>
-              </Link>
-            </li>
-            <li>
-              <Link legacyBehavior href="/about-us" passHerf>
-                <a>About Us</a>
-              </Link>
-            </li>
-          </ul>
-        </nav>
+      <div>
+        <div className="topNav">
+          <Image alt={'logo'} src={'/logo/Logo-k.png'} width={50} height={50} />
+          <nav>
+            <ul>
+              <li>
+                <Link legacyBehavior href="/" passHerf>
+                  <a>Home</a>
+                </Link>
+              </li>
+              <li>
+                <Link legacyBehavior href="/events" passHerf>
+                  <a>Events</a>
+                </Link>
+              </li>
+              <li>
+                <Link legacyBehavior href="/about-us" passHerf>
+                  <a>About Us</a>
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <h1>
+          Sed nulla sapien, ultricies sit amet posuere quis, commodo quis
+          sapien.
+        </h1>
       </div>
-      <h1>
-        Sed nulla sapien, ultricies sit amet posuere quis, commodo quis sapien.
-      </h1>
     </header>
   )
 }
