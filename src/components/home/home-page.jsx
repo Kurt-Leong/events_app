4abfc6fed7a0744cfff243b634d6527f470f6ac7
Kurt Liang <liangke1991@gmail.com>
2023-07-16 21:45:08 +1200

    figure user agent stylesheet

--- src/components/home/home-page.jsx
+++ src/components/home/home-page.jsx
@@ -5,19 +5,29 @@ import Link from 'next/link'
 function HomePage({ data }) {
   return (
     <div>
-      <main>
+      <div className="home_body">
         {data.map((event) => (
-          <Link key={event.id} href={`events/${event.id}`} passHref>
-            <Image
-              width={250}
-              height={'200'}
-              alt={event.title}
-              src={event.image}
-            />
-            <h2>{event.title}</h2> <p>{event.description}</p>
+          <Link
+            className="card"
+            key={event.id}
+            href={`events/${event.id}`}
+            passHref
+          >
+            <div className="image">
+              <Image
+                width={250}
+                height={'200'}
+                alt={event.title}
+                src={event.image}
+              />
+            </div>
+            <div className="content">
+              <h2>{event.title}</h2>
+              <p>{event.description}</p>
+            </div>
           </Link>
         ))}
-      </main>
+      </div>
     </div>
   )
 }
--- src/components/layout/Main-layout.jsx
+++ src/components/layout/Main-layout.jsx
@@ -6,7 +6,7 @@ function Mainlayout({ children }) {
   return (
     <div>
       <Header />
-      {children}
+      <main>{children}</main>
       <Footer />
     </div>
   )
