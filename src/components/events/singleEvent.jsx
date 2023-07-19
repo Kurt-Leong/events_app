8a18f4059e1ee431487445ba6a0ac4a2090179e4
Kurt Liang <liangke1991@gmail.com>
2023-07-20 11:09:42 +1200

    registered email write to data.json file

--- src/components/events/singleEvent.jsx
+++ src/components/events/singleEvent.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Image from 'next/image'
+import { useRouter } from 'next/router'
 
 function SingleEvent({ data }) {
-  const onSubmit = () => {}
+  const inputEmail = useRef()
+  console.log(inputEmail)
+
+  const router = useRouter()
+  console.log(router)
+
+  const onSubmit = async (e) => {
+    e.preventDefault()
+    const emailValue = inputEmail.current.value
+    console.log(emailValue)
+
+    const eventId = router?.query.id
+    try {
+      const response = await fetch('/api/email-registration', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email: emailValue, eventId }),
+      })
+
+      console.log('response is', response)
+      if (!response.ok) throw new Error(`Error: ${response.status}`)
+      const data = await response.json()
+      console.log('POST', data)
+      //POST fetch request
+      // body emailValue and the eventID
+    } catch (e) {
+      console.log('ERROR ', e)
+    }
+  }
   return (
     <div className="event_single_page">
       <h1>{data.title}</h1>
@@ -11,6 +42,7 @@ function SingleEvent({ data }) {
       <form onSubmit={onSubmit} className="email_registration">
         <label>Get Registered for this event</label>
         <input
+          ref={inputEmail}
           type="email"
           id="email"
           placeholder="Please insert your email here"
